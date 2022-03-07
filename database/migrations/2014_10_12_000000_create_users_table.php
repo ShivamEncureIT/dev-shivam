111194040f38c6627124131b074e11d0737fe040
ShivamEncureIT <100027351+ShivamEncureIT@users.noreply.github.com>
2022-03-07 11:41:56 +0530

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('status')->default('inactive');
+            $table->string('otp');
             $table->rememberToken();
             $table->timestamps();
         });
