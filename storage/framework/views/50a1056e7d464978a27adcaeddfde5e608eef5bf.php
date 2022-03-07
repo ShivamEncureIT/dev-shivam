8c09f65e076b2a2f98f4b5b252d01e687f72d43e
ShivamEncureIT <100027351+ShivamEncureIT@users.noreply.github.com>
2022-03-07 10:45:58 +0530

    Add files via upload
    Test

---
+++ storage/framework/views/50a1056e7d464978a27adcaeddfde5e608eef5bf.php
@@ -0,0 +1,25 @@
+
+  
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header"><?php echo e(__('Dashboard')); ?></div>
+  
+                <div class="card-body">
+                    <?php if(Session::get('success')): ?>
+                        <div class="alert alert-success" role="alert">
+                            <?php echo e(Session::get('success')); ?>
+
+                        </div>
+                    <?php endif; ?>
+  
+                    <h3>Welcome</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\practical\resources\views/dashboard.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/7ce5f18e9cd6a91c3d3425eb427e34d18ae51db4.php
@@ -0,0 +1,65 @@
+
+  
+<?php $__env->startSection('content'); ?>
+<div class="login-form">
+  <div class="container">
+      <div class="row justify-content-center">
+          <div class="col-md-8">
+              <div class="card">
+                  <div class="card-header">Login</div>
+                  <div class="card-body">
+
+                    <?php if(Session::get('success')): ?>
+                        <div class="alert alert-success" role="alert">
+                            <?php echo e(Session::get('success')); ?>
+
+                        </div>
+                    <?php endif; ?>
+  
+                      <form action="<?php echo e(route('login.post')); ?>" method="POST">
+                          <?php echo csrf_field(); ?>
+                          <div class="form-group row">
+                              <label for="email_address" class="col-md-4 col-form-label text-md-right">Email Address</label>
+                              <div class="col-md-6">
+                                  <input type="text" id="email_address" class="form-control" name="email" required />
+                                  <?php if($errors->has('email')): ?>
+                                      <span class="text-danger"><?php echo e($errors->first('email')); ?></span>
+                                  <?php endif; ?>
+                              </div>
+                          </div>
+  
+                          <div class="form-group row">
+                              <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+                              <div class="col-md-6">
+                                  <input type="password" id="password" class="form-control" name="password" required />
+                                  <?php if($errors->has('password')): ?>
+                                      <span class="text-danger"><?php echo e($errors->first('password')); ?></span>
+                                  <?php endif; ?>
+                              </div>
+                          </div>
+  
+                          <div class="form-group row">
+                              <div class="col-md-6 offset-md-4">
+                                  <div class="checkbox">
+                                      <label>
+                                          <input type="checkbox" name="remember"> Remember Me
+                                      </label>
+                                  </div>
+                              </div>
+                          </div>
+  
+                          <div class="col-md-6 offset-md-4">
+                              <button type="submit" class="btn btn-primary">
+                                  Login
+                              </button>
+                          </div>
+                      </form>
+                        
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\practical\resources\views/auth/login.blade.php ENDPATH**/ ?>
